4faac90633419179d7794eccf2879ac8f50635ba
Yolanda Wang <yolandaw998@deltalearns.ca>
2025-01-17 17:18:59 -0800

    it works!

--- include/robot-config.h
+++ include/robot-config.h
@@ -3,13 +3,6 @@ using namespace vex;
 extern brain Brain;
 
 // VEXcode devices
-extern smartdrive Drivetrain;
-extern motor ClawMotor;
-extern motor ArmMotor;
+extern controller Controller;
 
-/**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- * 
- * This should be called at the start of your int main function.
- */
 void  vexcodeInit( void );
